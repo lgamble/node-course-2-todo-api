d0d8ba40b7e0f5b0d8eab9fced7c506492fc2d20
Leanne Gamble <leanne@livingdatalabs.com>
2018-02-16 16:13:28 +0000

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -167,13 +167,21 @@ app.post('/users/login', (req, res) => {
 
     User.findByCredentials(body.email, body.password).then((user) => {
         // res.send(user);
-        return user.generateAuthToken().then((token) => { //return here so 400 will be called if there's an error
+        return user.generateAuthToken().then((token) => { //return here so 400 catch will be called if there's an error
             res.header('x-auth', token).send(user);
         });
     }).catch((e) => {
         res.status(400).send();
     });
 
+});
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }, () => {
+        res.status(400).send();
+    });
 });
 
 app.listen(port, () => {
